ec7ef7ada71149619981873a40f90679df6e1433
S Fujimoto <unicoshun@gmail.com>
2016-12-19 17:47:37 +0900

    ~4.2.4

--- app.js
+++ app.js
@@ -1,57 +1,36 @@
-const http = require('http');
-const path = require('path');
 const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
-app.set('views', path.resolve(__dirname, 'views'));
-app.set('view engine', 'ejs')
-
-const entries = [
-	{
-		title: "Another blog post",
-		body: "hey yo!",
-		published: new Date()
-	},
-	{
-		title: "Yet another blog post",
-		body: "hey yo again!",
-		published: new Date()
-	}
-];
-app.locals.entries = entries;
-
-app.use(logger("dev"));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', function(req, res) {
-	res.render('index');
+app.use((req, res, next) => {
+	console.log('Request IP: ' + req.url);
+	console.log('Request date: ' + new Date());
+	next();
 });
 
-app.get('/new-entry', function(req, res) {
-	res.render('new-entry');
-});
-
-app.post('/new-entry', function(req, res) {
-	if (!req.body.title || !req.body.body) {
-		res.status(400).send("Entries must have a title and a body");
-		return;
-	}
-	entries.push({
-		title: req.body.title,
-		body: req.body.body,
-		published: new Date()
+app.use((req, res, next) => {
+	let filePath = path.join(__dirname, 'static', req.url);
+	fs.stat(filePath, function(err, fileInfo){
+		if (err) {
+			next();
+			return;
+		}
+		if (fileInfo.isFile()) {
+			res.sendFile(filePath);
+		} else {
+			next();
+		}
 	});
-	res.redirect('/');
 });
 
 app.use((req, res) => {
-	res.status(404).render('404');
+	res.status(404);
+	res.send('File not found!');
 });
 
 
-
-http.createServer(app).listen(3000);
+app.listen(3000, () => {
+	console.log('App started on port 3000');
+});
